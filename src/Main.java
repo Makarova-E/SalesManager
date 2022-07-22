7cc1da1652c1c1d25a41990a27723134c3adf27b
Evgeniia Makarova <makarova-ovk@mail.ru>
2022-07-22 15:51:16 +0300

    Change for long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{5, 3, 6, 2});
+        SalesManager salesManager = new SalesManager(new long[]{5L, 3L, 6L, 2L});
 
         System.out.println(salesManager.max());
     }
